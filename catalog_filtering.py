1c6dc6099c30ee9114a9c59ba0acbcf26a9dd44f
JoaoDPassos <147557045+JoaoDPassos@users.noreply.github.com>
2024-06-12 14:58:44 -0400

    Successful xmatch 9 arcsec

--- catalog_filtering.py
+++ catalog_filtering.py
@@ -3,24 +3,27 @@
 
 def bandFilter(df, band, classStarMin = None, spreadMax = None, flag = False, maxMagError = None):
     queryString = createQueryString(band, classStarMin, spreadMax, flag, maxMagError)
-    print(queryString)
+    if queryString != '':
+        queryString += f' and WAVG_MAG_PSF_{band} > 0 and WAVG_MAG_PSF_{band} < 90'
+    else:
+        queryString += f'WAVG_MAG_PSF_{band} > 0 and WAVG_MAG_PSF_{band} < 90'
     return df.query(queryString)
 
 #Generates the string for .query method
     
 def createQueryString(band, classStarMin, spreadMax, flag, maxMagError):
-    queryString = f'WAVG_MAG_PSF_{band} > 0 and WAVG_MAG_PSF_{band} < 90'
+    queryString = ''
     if classStarMin != None:
-        queryString += ' and '
+        if queryString != '': queryString += ' and '
         queryString += f'CLASS_STAR_{band} > {classStarMin}'
     if spreadMax != None:
-        queryString += ' and '
+        if queryString != '': queryString += ' and '
         queryString += f'SPREAD_MODEL_{band} < {spreadMax}'
     if flag:
-        queryString += ' and '
+        if queryString != '': queryString += ' and '
         queryString += f'FLAGS_{band} < 4'
     if maxMagError != None:
-        queryString += ' and '
+        if queryString != '': queryString += ' and '
         queryString += f'WAVG_MAGERR_PSF_{band} < {maxMagError}'
     return queryString
 
@@ -34,8 +37,11 @@ def multiBandFilterStrict(df, bandList, classStarMin = None, spreadMax = None, f
 #Filters through multiple bands, makes sure objects satisfy filters for AT LEAST 1 band (or)
 
 def multiBandFilterLenient(df, bandList, classStarMin = None, spreadMax = None, flag = False, maxMagError = None):
-    queryMultiString = ''
+    queryString = ''
+    strictFilters = ''
     for band in bandList:
-        queryMultiString += f'({createQueryString(band, classStarMin, spreadMax, flag, maxMagError)}) or '
-    queryMultiString = queryMultiString[:-3]
-    return df.query(queryMultiString)
+        queryString += f'({createQueryString(band, classStarMin, spreadMax, flag, maxMagError)}) or '
+        strictFilters += f'WAVG_MAG_PSF_{band} > 0 and WAVG_MAG_PSF_{band} < 90 and ' #False values must always be filtered
+    queryString = queryString[:-3]
+    finalString = strictFilters + f'({queryString})' if queryString != '' else strictFilters
+    return df.query(finalString)
